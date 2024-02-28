b479db2a79d5258b743b651cda01c9e3d4b81b83
Aniket Ghosh <ghoshaniket1000@gmail.com>
2024-02-28 14:57:34 +0530

    changed css of sendmoney page

--- frontend/src/pages/SendMoney.jsx
+++ frontend/src/pages/SendMoney.jsx
@@ -1,4 +1,4 @@
-import { json, useSearchParams } from 'react-router-dom';
+import { useSearchParams } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import { useState } from 'react';
 
@@ -10,27 +10,26 @@ function SendMoney() {
     const [amount, setAmount] = useState(0);
     const navigate = useNavigate();
 
-    return <div class="flex justify-center h-screen bg-gray-100">
+    return <div className="flex justify-center h-screen bg-black text-white">
         <div className="h-full flex flex-col justify-center">
             <div
-                class="border h-min text-card-foreground max-w-md p-4 space-y-8 w-96 bg-white shadow-lg rounded-lg"
+                class="h-min text-card-foreground max-w-md p-4 w-96 bg-gray-600 shadow-lg rounded-lg"
             >
-                <div class="flex flex-col space-y-1.5 p-6">
-                <h2 class="text-3xl font-bold text-center">Send Money</h2>
+                <div className="flex flex-col p-6">
+                <h2 className="text-3xl font-bold text-center">Send Money</h2>
                 </div>
-                <div class="p-6">
-                <div class="flex items-center space-x-4">
-                    <div class="w-12 h-12 rounded-full bg-green-500 flex items-center justify-center">
-                    <span class="text-2xl text-white">{name[0].toUpperCase()}</span>
+                <div className="p-6">
+                <div className="flex items-center">
+                    <div className="w-12 h-12 rounded-full bg-red-300 flex items-center justify-center">
+                    <span className="text-2xl text-white ml">{name[0].toUpperCase()}</span>
                     </div>
-                    <h3 class="text-2xl font-semibold">{name}</h3>
+                    <h3 className="text-2xl font-semibold ml-2">{name}</h3>
                 </div>
-                <div class="space-y-4">
-                    <div class="space-y-2">
+                <div>
+                    <div className="mt-3 mb-4">
                     <label
-                        class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-                        for="amount"
-                    >
+                        className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                        for="amount">
                         Amount (in Rs)
                     </label>
                     <input
@@ -38,7 +37,7 @@ function SendMoney() {
                             setAmount(Number(e.target.value));
                         }}
                         type="number"
-                        class="flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-sm"
+                        className=" mt-2 flex h-10 w-full rounded-lg border border-input bg-background px-3 py-2 text-sm"
                         id="amount"
                         placeholder="Enter amount"
                     />
@@ -61,7 +60,7 @@ function SendMoney() {
                             alert("Transaction failed")
                         }
                         navigate("/dashboard");
-                        }} class="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
+                        }} className="justify-center rounded-md text-sm font-medium ring-offset-background transition-colors h-10 px-4 py-2 w-full bg-green-500 text-white">
                         Initiate Transfer
                     </button>
                 </div>
